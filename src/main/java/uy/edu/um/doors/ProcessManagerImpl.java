4c97236f249babb25dfec35c4e14590e26d3ae3f
sabrii123 <smures@correo.um.edu.uy>
2026-06-08 10:44:28 -0300

    Changes by memim

--- src/main/java/uy/edu/um/doors/ProcessManagerImpl.java
+++ src/main/java/uy/edu/um/doors/ProcessManagerImpl.java
@@ -1,10 +1,16 @@
 package uy.edu.um.doors;
+
 import uy.edu.um.doors.entities.Process;
 import uy.edu.um.tad.heap.MyHeap;
 import uy.edu.um.tad.queue.EmptyQueueException;
 import uy.edu.um.tad.queue.MyQueue;
 import uy.edu.um.tad.stack.MyStack;
+import uy.edu.um.tad.hash.MyHash;
 import uy.edu.um.doors.entities.*;
+import java.io.BufferedReader; //leer texto linea por linea
+import java.io.FileReader; //abrir archivo para lectura
+import java.io.IOException; //errores al leer archivos
+import java.util.List;
 
 public class ProcessManagerImpl implements ProcessManager{
 
@@ -13,13 +19,52 @@ public class ProcessManagerImpl implements ProcessManager{
     private MyHeap<Process> pendingProcesses;
     private Process runningProcess;
     private MyStack<Process> finishedProcesses;
+    private MyHash<Integer, User> users;
 
     private static final int MAX_FINISHED = 3; // o el valor que les pidan/definan
     GestorArchivos ga= new GestorArchivos();
     @Override
     public void loadProcessAndUserData(String processCsvPath, String usersCsvPath) {
-        Process proceso_nuevo = new Process();
+        cargarUsuarios(usersCsvPath);
+        cargarProcesos(processCsvPath);
+    }
+    private void cargarUsuarios(String usersCsvPath) {
+        try (BufferedReader br = new BufferedReader(new FileReader(usersCsvPath))) {
+            String linea;
+            br.readLine();
+            while ((linea = br.readLine()) != null) {
+                if (linea.trim().isEmpty()) {
+                    continue;
+                }
+                String[] datos = linea.split(",");
+                int iud = Integer.parseInt(datos[0].trim());
+                String alias = datos[1].trim();
+                String type = datos[2].trim();
+                User user = new User(iud, alias, type);
+                users.put(iud, user);
+            }
+        } catch (IOException e) {
+            System.out.println("Error cargando los usuarios");
+        }
+    }
 
+    private void cargarProcesos(String processCsvPath){
+        try (BufferedReader br = new BufferedReader(new FileReader(processCsvPath))) {
+            String linea;
+            br.readLine();
+            while ((linea = br.readLine()) != null) {
+                if (linea.trim().isEmpty()) {
+                    continue;
+                }
+                String[] datos = linea.split(",");
+                int pid = Integer.parseInt(datos[0].trim());
+                String name = datos[1].trim();
+
+
+            }
+        } catch (IOException e) {
+            System.out.println("Error cargando los procesos");
+        }
     }
 
     @Override
